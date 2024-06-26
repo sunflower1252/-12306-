4b9e825e0c676468d32f4a4546b9567d07e7c742
yangguang <3141219704@qq.com>
2024-06-26 17:57:41 +0800

    初始化member模块

--- member/src/main/java/com/sanzuniao/member/MemberApplication.java
+++ member/src/main/java/com/sanzuniao/member/MemberApplication.java
@@ -1,7 +1,10 @@
 package com.sanzuniao.member;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 /**
  * @author yangguang
@@ -9,8 +12,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MemberApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
     }
 
 }
--- member/src/main/java/com/sanzuniao/member/controller/TestController.java
+++ member/src/main/java/com/sanzuniao/member/controller/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
-    @GetMapping("/helloWorld")
+    @GetMapping("/hello")
     public String test(){
         return "helloWorld";
     }
